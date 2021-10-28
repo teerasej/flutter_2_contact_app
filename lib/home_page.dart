81b3a6c63ae819d906a3f58ae95487be1c6e18db
Teerasej Jiraphatchandej <teerasej@gmail.com>
2021-10-28 22:02:27 +0700

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,15 @@
+// Copyright (c) 2021 Teerasej Jiraphatchandej
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/my_app.dart
@@ -0,0 +1,22 @@
+// Copyright (c) 2021 Teerasej Jiraphatchandej
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+import 'package:flutter/material.dart';
+import 'package:test_app/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Nextflow Contact App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
