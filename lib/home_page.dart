db69b8013b077f96f3f0435f66315f620656f7bc
Teerasej Jiraphatchandej <teerasej@gmail.com>
2021-10-28 22:05:16 +0700

    switch home page to stateful widget

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,15 +5,16 @@
 
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Contacts'),
-      ),
-    );
+    return Container();
   }
 }
